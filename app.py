495e6ca31fda8a0443a90db578eae737a093c1cd
Sam Dvorin <samdvorin@Sams-MacBook-Pro-2.local>
2023-10-29 22:13:25 -0400

    basic template

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
-app = Flask(__name__)
+app = Flask(__name)
 
 @app.route('/')
 def hello():
     return "Hello from Flask!"
+
+@app.route('/api/data')
+def get_data():
+    data = {"message": "Data from Flask"}
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- movie-quiz/src/app/page.js
+++ movie-quiz/src/app/page.js
@@ -4,91 +4,57 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <div className={styles.Home}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          <strong>Movie Quiz</strong>
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      </div>
+      <div className={styles.description.center}>
+        <p>
+          New Game
+        </p>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.description.center}>
+        <p>
+          Current Score
+        </p>
+      </div>
+
+      <div className={styles.description.center}>
+        <p>
+          High Scores
+        </p>
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <div className={styles.card}>
+          <img src="https://via.placeholder.com/150" alt="Poster 1" />
+        </div>
+        <div className={styles.card}>
+          <img src="https://via.placeholder.com/150" alt="Poster 2" />
+        </div>
+        <div className={styles.card}>
+          <img src="https://via.placeholder.com/150" alt="Poster 3" />
+        </div>
+        <div className={styles.card}>
+          <img src="https://via.placeholder.com/150" alt="Poster 4" />
+        </div>
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          Sound 1
+        </div>
+        <div className={styles.card}>
+          Sound 2
+        </div>
+        <div className={styles.card}>
+          Sound 3
+        </div>
+        <div className={styles.card}>
+          Sound 4
+        </div>
       </div>
     </main>
   )
