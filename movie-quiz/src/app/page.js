59a15b2bbdcab35233d8a3e742fa3d8c4df813f9
Xiang <dengx@bu.edu>
2023-11-10 11:16:00 -0500

    added CSS formatting

--- movie-quiz/src/app/page.js
+++ movie-quiz/src/app/page.js
@@ -3,16 +3,60 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
+    <div>
+      <div className={styles.header}>
+        <h1>Movie Quiz</h1>
+      </div>
+
+      <div className={styles.nav}>
+        <ul>
+          <li><button onClick="loginFunction()">Login</button></li>
+          <li><button onClick="newgameFunction()">New Game</button></li>
+        </ul>
+      </div>
+
+      <div className={styles.main}>
+        <div className={styles.playerScores}>
+          <ul>
+            <li><p>Current Score: </p><a href="#"> </a></li>
+            <li><p>High Score: </p><a href="#"> </a></li>
+          </ul>
+        </div>
+
+        <div className={styles.moviePosters}>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 1" />
+          </a>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 2" />
+          </a>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 3" />
+          </a>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 4" />
+          </a>
+        </div>
+
+        <div className={styles.playSound}>
+          <button onClick="soundtrackFunction()">Play Soundtrack</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+{/* <div id="player-scores">
+        <li><p>Current Score</p></li>
+      </div>
     <main className={styles.main}>
       <div className={styles.Home}>
-        <p>
-          <strong>Movie Quiz</strong>
-        </p>
       </div>
       <div className={styles.description.center}>
-        <p>
-          New Game
-        </p>
+        <button onClick="loginFunction()">Login</button>
+      </div>
+
+      <div className={styles.description.center}>
+        <button onClick="newgameFunction()">New Game</button>
       </div>
 
       <div className={styles.description.center}>
@@ -25,9 +69,9 @@ export default function Home() {
         <p>
           High Scores
         </p>
-      </div>
+      </div> */}
 
-      <div className={styles.grid}>
+      {/* <div className={styles.grid}>
         <div className={styles.card}>
           <a href="#" onClick="myFunction()">
             <img src="https://via.placeholder.com/150" alt="Poster 1" />
@@ -54,16 +98,5 @@ export default function Home() {
         <div className={styles.card}>
           Sound 1
         </div>
-        {/* <div className={styles.card}>
-          Sound 2
-        </div>
-        <div className={styles.card}>
-          Sound 3
-        </div>
-        <div className={styles.card}>
-          Sound 4
-        </div> */}
       </div>
-    </main>
-  )
-}
+    </main> */}
