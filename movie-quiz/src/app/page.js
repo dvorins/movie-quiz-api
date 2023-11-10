0c7d219576d80c1f3967a410dce5af782978db64
Sam Dvorin <samdvorin@Sams-MacBook-Pro-2.local>
2023-11-10 11:33:02 -0500

    first merging of front and back

--- movie-quiz/src/app/page.js
+++ movie-quiz/src/app/page.js
@@ -3,93 +3,100 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
+    <div>
+      <div className={styles.header}>
+        <h1>Movie Quiz</h1>
+      </div>
+
+      <div className={styles.nav}>
+        <ul>
+          <li><button onClick="loginFunction()">Login</button></li>
+          <li><button onClick="newgameFunction()">New Game</button></li>
+        </ul>
+      </div>
+
+      <div className={styles.main}>
+        <div className={styles.playerScores}>
+          <ul>
+            <li><p>Current Score: </p><a href="#"> </a></li>
+            <li><p>High Score: </p><a href="#"> </a></li>
+          </ul>
+        </div>
+
+        <div className={styles.moviePosters}>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 1" />
+          </a>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 2" />
+          </a>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 3" />
+          </a>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 4" />
+          </a>
+        </div>
+
+        <div className={styles.playSound}>
+          <button onClick="soundtrackFunction()">Play Soundtrack</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+{/* <div id="player-scores">
+        <li><p>Current Score</p></li>
+      </div>
     <main className={styles.main}>
-      <div className={styles.description}>
+      <div className={styles.Home}>
+      </div>
+      <div className={styles.description.center}>
+        <button onClick="loginFunction()">Login</button>
+      </div>
+
+      <div className={styles.description.center}>
+        <button onClick="newgameFunction()">New Game</button>
+      </div>
+
+      <div className={styles.description.center}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Current Score
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+      </div>
+
+      <div className={styles.description.center}>
+        <p>
+          High Scores
+        </p>
+      </div> */}
+
+      {/* <div className={styles.grid}>
+        <div className={styles.card}>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 1" />
+          </a>
+        </div>
+        <div className={styles.card}>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 2" />
+          </a>
+        </div>
+        <div className={styles.card}>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 3" />
+          </a>
+        </div>
+        <div className={styles.card}>
+          <a href="#" onClick="myFunction()">
+            <img src="https://via.placeholder.com/150" alt="Poster 4" />
           </a>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className={styles.card}>
+          Sound 1
+        </div>
       </div>
-    </main>
-  )
-}
+    </main> */}
